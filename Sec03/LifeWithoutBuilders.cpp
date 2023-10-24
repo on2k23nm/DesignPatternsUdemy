7cae0f51f03c0d11d32e66b0e2c48155644b8647
Onkar N. Mahajan <onnmprof@gmail.com>
2023-10-24 18:21:17 +0530

    Sec03, lec13, Fluent interface

--- Sec03/LifeWithoutBuilders.cpp
+++ Sec03/LifeWithoutBuilders.cpp
@@ -19,6 +19,7 @@ using namespace std;
 start creating object oriented structures which actually represent the
 different part of the HTML that you are building.
 */
+struct HtmlBuilder;
 
 // element is just going to represent a tag,including anything inside the tag "<name>text</name>"
 // this is just a utility class for modelling an HTML element
@@ -49,6 +50,8 @@ struct HtmlElement
 
         return oss.str(); 
     }
+
+    static HtmlBuilder build(string rootName);
 }; 
 // Now, this gets you the structure, now the interesting thing is how do you actually
 // build up these elements ? This is where the builder pattern comes in.
@@ -65,9 +68,10 @@ struct HtmlBuilder
     }
 
     // utility functions to add elements
-    void addChild(string childName, string childText)
+    HtmlBuilder& addChild(string childName, string childText)
     {
         root.elements.emplace_back(childName, childText);
+        return *this; // For fluent-interface 
     }
 
     // we want str() function now which returns a string representation of whatever
@@ -77,6 +81,11 @@ struct HtmlBuilder
         return root.str(); 
     }
 };
+
+HtmlBuilder HtmlElement::build(string rootName)
+{
+    return HtmlBuilder(rootName);
+}
 
 int main(int argc, char *argv[])
 {
@@ -112,8 +121,31 @@ int main(int argc, char *argv[])
     HtmlBuilder builder("ul");
     builder.addChild("li", "hello");
     builder.addChild("li", "world");
-
     cout << builder.str();
+
+    /*
+    One of the improvements to our implementation
+    of the builder is something called a "Fluent Interface"
+    We've got 2 addChild() calls one after another. What
+    if we can do it like below -
+    */
+
+    HtmlBuilder builder1("ul");
+    builder1.addChild("li", "hello").addChild("li", "world"); // fluent interface
+    // // ^^^^^^^^^^ to make a fluent interface we simply change the return type of addChild to pointer/ref
+    // // to HtmlBuilder, we can keep calling addChild after that to inf. 
+    // // This is particularly convenient in builders because - 
+    // // 1) Sometimes builders you make want to do several very similar things
+    // // 2) You might want to specify how the chain of build commands can be constructed.
+    cout << builder1.str(); 
+
+    // Another thing you can do is to give hint to the consumer/client that they should
+    // actually be using the Builder.Up to you how you can do this, but one way is to use static
+    // funtion in HTMLElement, see above (the function is not inline because it is giving compilation issues)
+    // After you do all that, you can say something like this - 
+    auto builder2 = HtmlElement::build("ul");
+    builder2.addChild("li", "hello").addChild("li", "world");
+    cout << builder2.str();
 
     return 0;
 }
